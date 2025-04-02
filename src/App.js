a91ccb04bd9220a18fcd2111514c4a0256839c90
lirasik <liras966@gmail.com>
2025-04-02 06:27:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FixedSizeList as List } from "react-window";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { faker } from "@faker-js/faker/locale/en";
+import "./styles.css";
 
-function App() {
+const generateUsers = (count) => {
+  return Array.from({ length: count }, (_, id) => ({
+    id,
+    name: faker.person.firstName(),
+    surname: faker.person.lastName(),
+    age: faker.number.int({ min: 18, max: 80 }),
+    email: faker.internet.email(),
+  }));
+};
+
+const userSlice = createSlice({
+  name: "users",
+  initialState: generateUsers(1000000),
+  reducers: {
+    updateUser: (state, action) => {
+      const index = state.findIndex((user) => user.id === action.payload.id);
+      if (index !== -1) state[index] = action.payload;
+    },
+  },
+});
+
+const store = configureStore({ reducer: { users: userSlice.reducer } });
+
+const UserList = ({ onSelect }) => {
+  const users = useSelector((state) => state.users);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <List
+      height={500}
+      width={300}
+      itemSize={50}
+      itemCount={users.length}
+      className="user-list"
+    >
+      {({ index, style }) => (
+        <div
+          key={users[index].id}
+          className="user-item"
+          style={{
+            ...style,
+            height: "auto",
+          }}
+          onClick={() => onSelect(users[index])}
         >
-          Learn React
-        </a>
-      </header>
+          <p style={{ margin: 0 }}>
+            {users[index].name} {users[index].surname}
+          </p>
+        </div>
+      )}
+    </List>
+  );
+};
+
+const UserDetails = ({ user, onSave }) => {
+  const [form, setForm] = useState(user);
+  const dispatch = useDispatch();
+
+  useEffect(() => setForm(user), [user]);
+
+  return (
+    <div className="user-details">
+      <div className="fullName">
+        <div className="name">
+          <label>Имя</label>
+          <input
+            type="text"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+          />
+        </div>
+        <div className="surname">
+          <label>Фамилия</label>
+          <input
+            type="text"
+            value={form.surname}
+            onChange={(e) => setForm({ ...form, surname: e.target.value })}
+          />
+        </div>
+      </div>
+      <label>Почта</label>
+      <input
+        type="text"
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <label>Возвраст</label>
+      <input
+        type="text"
+        value={form.age}
+        onChange={(e) => setForm({ ...form, age: e.target.value })}
+      />
+      <button
+        onClick={() => {
+          dispatch(userSlice.actions.updateUser(form));
+          onSave();
+        }}
+      >
+        Сохранить
+      </button>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  return (
+    <Provider store={store}>
+      <div className="app-container">
+        <UserList onSelect={setSelectedUser} />
+        {selectedUser && (
+          <UserDetails
+            user={selectedUser}
+            onSave={() => setSelectedUser(null)}
+          />
+        )}
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
